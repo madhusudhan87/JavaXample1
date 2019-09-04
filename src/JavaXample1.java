7bf3268b832303dc47bb640fb4c298dce8d50d50
vedang <vedang@whitebox-learning.com>
2019-09-03 17:04:50 -0700

    i added another statement at 7th line

--- src/JavaXample1.java
+++ src/JavaXample1.java
@@ -4,6 +4,7 @@ public class JavaXample1 {
 	public static void main(String[] args) {
 		System.out.println("Hi everyone");
         System.out.println("im from eclipse");
+	System.out.println("to jenkins");
 	}
 
 }
